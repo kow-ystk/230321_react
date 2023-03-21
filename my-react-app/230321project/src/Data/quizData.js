903d9bcdcce9d311b4c42ee761ace81ae479d286
Koki Kamezono <kow1ag25@gmail.com>
2023-03-21 17:45:18 +0900

    クイズアプリを作成

---
+++ my-react-app/230321project/src/Data/quizData.js
@@ -0,0 +1,18 @@
+const quizData = [
+  {
+    question: 'Reactはどの言語で書かれているでしょうか？',
+    options: ['JavaScript', 'Ruby', 'Python', 'C++'],
+    answer: 'JavaScript',
+  },
+  {
+    question: 'HTMLのh1タグは何を表しますか？',
+    options: ['見出し1', '見出し2', '見出し3', '見出し4'],
+    answer: '見出し1',
+  },
+  {
+    question: 'CSSのbox-sizingプロパティの初期値は何ですか？',
+    options: ['content-box', 'border-box', 'padding-box', 'margin-box'],
+    answer: 'content-box',
+  },
+];
+export default quizData;
